cc8dac359f5a01b0a785057b00f76911c9204425
Mark <didenko.m@hi-tech.md>
2025-11-20 13:11:54 +0000

    recreate app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
-const CONFIG = require('../config.js');
+const CONFIG = require('./config.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
